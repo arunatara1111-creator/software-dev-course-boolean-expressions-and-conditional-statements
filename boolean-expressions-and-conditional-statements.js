d93d69ad32b7e59ae347781352f43dc7ff4b2af1
Aruna <arunatara1111@gmail.com>
2026-04-30 09:50:03 -0500

    first commit

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -25,18 +25,44 @@ Paste the following code into your editor:
 */
 
 const readline = require('readline-sync');
-
+const hasSword=true;
+const hasCompass=true;
 const hasTorch = true;
 const hasMap = false;
-
+const health=true;//health is true means it is in good condition
+let cave=true;
+let river=true;
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
+if (choice === "mountains" && hasTorch && health) {
+   if(hasSword && hasCompass)
+    console.log("You safely navigate through the dark mountains because of torch.Sword and compass help to find direction, sword used for safety which helps to protect from wild animals or snakes");
+  else if(hasSword ||!hasCompass) 
+    console.log("You safely navigate through the dark mountains,but need to be careful for directions");
+  else if(!hasSword || hasCompass)
+    console.log("You safely navigate through the dark mountains,but need to be careful on wild animals or snakes");
+  else
+  console.log("You safely navigate through the dark mountains,but need to be careful for directions and wild animals or snakes");
+} 
+else if (choice === "mountains" && !hasTorch) 
   console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
+
+  else if(choice==="mountains" && hasTorch && !health)
+    console.log("its not safe to go to mountains if health is not good");
+
+else if (choice === "village" && hasCompass|| !hasMap) {
+  if(health)
+  {
+    console.log("You see two paths: one leads to the cave in the village, the other to the river in the village.");
+    const choice1 = readline.question("Do you go to the 'cave' or the 'river'?");
+    if(choice1==="cave")
+      console.log("You find your way to the cave in the village.");
+    else
+    console.log("You find your way to the river in the village.");
+
+  }
+  else
   console.log("You find your way to the village.");
 } else {
   console.log("You get lost and wander aimlessly.");
